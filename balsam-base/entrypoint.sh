1377894497ac6c7b2a4affd85f776cbb4e01152a
vsoch <vsoch@users.noreply.github.com>
2023-06-09 11:46:52 -0600

    ensure we migrate without gunicorn
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- balsam-base/entrypoint.sh
+++ balsam-base/entrypoint.sh
@@ -2,6 +2,6 @@
 
 export BALSAM_LOG_DIR="/balsam/log"
 mkdir -p $BALSAM_LOG_DIR
-gunicorn balsam server migrate || echo "gunicorn balsam server migrate not successful"
+balsam server migrate || echo "balsam server migrate not successful"
 gunicorn --print-config -c /balsam/gunicorn.conf.py balsam.server.main:app
 exec gunicorn -c /balsam/gunicorn.conf.py balsam.server.main:app
